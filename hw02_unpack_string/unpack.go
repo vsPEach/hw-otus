2a6a15e884d0205ff1602da0ce4887fa758bfc19
vsPEach <pers1kwtf@gmail.com>
2022-12-10 13:54:36 +0300

    hw02 was complete

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,40 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	res := new(strings.Builder)
+	input := []rune(str)
+	if len(input) != 0 && unicode.IsDigit(input[0]) {
+		return "", ErrInvalidString
+	}
+	if str == "" {
+		return "", nil
+	}
+	for i := 0; i < len(input)-1; i++ {
+		if unicode.IsDigit(input[i]) && unicode.IsDigit(input[i+1]) {
+			return "", ErrInvalidString
+		}
+		if !unicode.IsDigit(input[i]) && unicode.IsDigit(input[i+1]) {
+			count, err := strconv.Atoi(string(input[i+1]))
+			if err != nil {
+				return "", err
+			}
+			substr := strings.Repeat(string(input[i]), count)
+			res.WriteString(substr)
+			continue
+		} else if !unicode.IsDigit(input[i]) {
+			res.WriteString(string(input[i]))
+		}
+	}
+	if !unicode.IsDigit(input[len(input)-1]) {
+		res.WriteRune(input[len(input)-1])
+	}
+	return res.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,7 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "abcd5", expected: "abcddddd"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
